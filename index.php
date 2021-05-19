87de09afa73dd4414ddf8021053fa936c4fb8e9a
Kleyton <KleytonCaires@gmail.com>
2021-05-19 14:39:09 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br" class="no-js">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover">
+        <meta name="format-detection" content="telephone=no">
+        <link rel="stylesheet" href="assets/css/style.scss">
+    </head>
+
+    <body>
+
+        <script src="assets/js/vendor.js"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+
+</html>
