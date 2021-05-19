b516cb51c596066668bc0098560405bb36cb4ace
Kleyton <KleytonCaires@gmail.com>
2021-05-19 14:42:27 -0300

    Initial commit

--- partials/header.php
+++ partials/header.php
@@ -1,22 +1,21 @@
 <header class="headerMenu shadow" id="headerBar">
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
         <div class="container">
-            <a class="navbar-brand" href="<?php echo site_url(); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="<?php echo get_bloginfo('name'); ?>">
+            <a class="navbar-brand" href="">
+                <img src="assets/img/logo.svg" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarHeader">
-                <?php
-                wp_nav_menu(array(
-                    'theme_location'        => 'header-nav',
-                    'container'             => 'nav',
-                    'menu_class'            => 'navbar-nav',
-                    'list_item_class'       => 'nav-item',
-                    'link_class'            => 'nav-link'
-                ));
-                ?>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
--- partials/sidebar.php
+++
@@ -1,7 +0,0 @@
-<aside class="sidebar" role="complementary">
-    <?php get_template_part('searchform'); ?>
-
-    <div class="sidebar-widget">
-        <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar')) ?>
-    </div>
-</aside>
