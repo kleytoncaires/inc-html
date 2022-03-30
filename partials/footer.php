adf4121345ff73f81dd566552a766725a04f18f9
Kleyton Caires <kleytoncaires@gmail.com>
2022-03-30 11:36:37 -0300

    feat: plugin updates

--- partials/footer.php
+++ partials/footer.php
@@ -7,7 +7,7 @@
         <script type="text/javascript" src="./assets/js/vendor/jquery.min.js"></script>
         <script type="text/javascript" src="./assets/js/vendor/bootstrap.bundle.min.js"></script>
         <script type="text/javascript" src="./assets/js/vendor/jquery.mask.min.js"></script>
-        <script type="text/javascript" src="./assets/js/vendor/slick.min.js"></script>
+        <script type="text/javascript" src="./assets/js/vendor/swiper-bundle.min.js"></script>
         <script type="text/javascript" src="./assets/js/vendor/jquery.fancybox.min.js"></script>
         <script src="./assets/js/custom.js"></script>
     </body>
